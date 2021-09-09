1782e7fb4bb9a3f59cf6fa6fa71a3aa861e60270
Robson Paiva <paivawork@hotmail.com>
2021-09-09 14:47:06 -0300

    Configurando o Prism

---
+++ SCommerce.Main/Common/PageTokens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCommerce.Main.Common
+{
+    public static class PageTokens
+    {
+        public const string ProductDetailsPage = "ProductDetails";
+    }
+}
