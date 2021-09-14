cef6c9289e1e82943f8ef88b102cca3c69e04022
Robson Paiva <paivawork@hotmail.com>
2021-09-14 14:41:57 -0300

    Ajuste

--- SCommerce.Main/ViewModels/ProductDetailsPageViewModel.cs
+++ SCommerce.Main/ViewModels/ProductDetailsPageViewModel.cs
@@ -63,20 +63,9 @@ namespace SCommerce.Main.ViewModels
         } 
         #endregion
 
-
         public ProductDetailsPageViewModel(IProductService productService)
         {
-            Title = "Produto 1";
-            Description = "Não é uma descrição ideal para este produto. Mas é a que tem :)";
-            Price = 99.99856;
-            Rating = 4;
-            Images = new List<string>
-            {
-                "ms-appx:///Assets/Images/shirt1.jpg",
-                "ms-appx:///Assets/Images/shirt2.jpg",
-                "ms-appx:///Assets/Images/shirt3.jpg"
-            };
-            SelectedImage = Images[0];
+            this.productService = productService;
         }
 
         public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
