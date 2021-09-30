99f38884e4811013e847758156940cefc816e855
Robson Paiva <paivawork@hotmail.com>
2021-09-30 11:53:51 -0300

    Final Control - Part2

---
+++ SCommerce.Main/Controls/SpaceBetweenPanel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace SCommerce.Main.Controls
+{
+    public class SpaceBetweenPanel : Panel
+    {
+        double itemWidth = 0;
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var remainingSize = availableSize;
+            var size = new Size(0, 0);
+
+            foreach (var item in Children)
+            {
+                item.Measure(remainingSize);
+
+                if (item.DesiredSize.Height > size.Height)
+                    size.Height = item.DesiredSize.Height;
+
+                size.Width += item.DesiredSize.Width;
+                remainingSize.Width -= item.DesiredSize.Width;
+            }
+
+            itemWidth = size.Width;
+            return size;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var size = new Size();
+            double spaceBetween = (finalSize.Width - itemWidth) / (Children.Count - 1);
+
+            foreach (var item in Children)
+            {
+                if (item.DesiredSize.Height > size.Height)
+                    size.Height = item.DesiredSize.Height;
+
+                item.Arrange(new Rect(size.Width, 0, item.DesiredSize.Width, item.DesiredSize.Height));
+
+                size.Width += item.DesiredSize.Width + spaceBetween;
+            }
+
+            return size;
+        }
+
+    }
+}
