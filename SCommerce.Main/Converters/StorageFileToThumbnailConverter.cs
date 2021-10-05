4a2a2f3ce37c7ab2d0b27922c3c2b188f4fc05e8
Robson Paiva <paivawork@hotmail.com>
2021-10-05 20:12:59 -0300

    Show image from StorageFile

---
+++ SCommerce.Main/Converters/StorageFileToThumbnailConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SCommerce.Main.Converters
+{
+    public class StorageFileToThumbnailConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is StorageFile file)
+            {
+                using (var stream = file.OpenReadAsync().AsTask().Result)
+                {
+                    var bi = new BitmapImage();
+                    bi.SetSource(stream);
+                    return bi;
+                }
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
